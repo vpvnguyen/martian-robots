89d0df798a9467f2e4e24623823b4a81269da3b2
Vincent Nguyen <vpvnguyen@gmail.com>
2021-04-12 00:43:50 -0700

    WIP - instructions form to handle user's input; need to save robot commands to state

--- src/instructions/InstructionsForm.tsx
+++ src/instructions/InstructionsForm.tsx
@@ -1,1 +1,58 @@
-export const InstructionsForm = () => <div>Instructions</div>;
+import { useState } from "react";
+import { useGridDimensionsContext } from "./InstructionsContext";
+import InstructionsMapper from "./InstructionsMapper";
+import { InstructionsFormError } from "./InstructionsErrorHandling";
+
+const initialInputStringState: string = "";
+
+const InstructionsForm = () => {
+  const [inputString, setInputString] = useState(initialInputStringState);
+  const { setGridDimensions } = useGridDimensionsContext();
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    try {
+      const mappedInstructions = new InstructionsMapper(inputString);
+      const gridDimensions = mappedInstructions.getGridDimensions();
+      const robotCommands = mappedInstructions.getRobotCommands();
+
+      // TODO:
+      // save robotCommands to state
+
+      setGridDimensions({
+        width: gridDimensions[0],
+        height: gridDimensions[1],
+      });
+    } catch (error) {
+      console.error("error:", error);
+      throw new InstructionsFormError(
+        "There was an issue submitting the form!"
+      );
+    }
+  };
+
+  const handleChange = (event: any) => {
+    setInputString(event.target.value);
+  };
+
+  return (
+    <div className="w-full p-10">
+      <label htmlFor="instructions">Instructions:</label>
+      <textarea
+        className="p-4 placeholder-blueGray-300 text-blueGray-600 bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full"
+        rows={10}
+        name="instructions"
+        value={inputString}
+        onChange={handleChange}
+      />
+      <button
+        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default InstructionsForm;
