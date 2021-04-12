5fc87fc61838bf62ecc70c4b4eb1e6d74147b0b4
Vincent Nguyen <vpvnguyen@gmail.com>
2021-04-12 00:50:02 -0700

    abstracted mars grid component into it's own file; use grid dimensions context to provide width and height of grid from instructions form and render mars grid

---
+++ src/mars/MarsGrid.tsx
@@ -0,0 +1,56 @@
+import { useGridDimensionsContext } from "../instructions/InstructionsContext";
+
+const MarsGrid = () => {
+  const {
+    gridDimensions: { width, height },
+  } = useGridDimensionsContext();
+
+  const createCoordinates = (width: number, height: number) => {
+    const coordinates = [];
+    for (let yPosition = 0; yPosition < height; yPosition++) {
+      for (let xPosition = 0; xPosition < width; xPosition++) {
+        coordinates.push(`${xPosition},${yPosition}`);
+      }
+    }
+    return coordinates;
+  };
+
+  const createGrid = (coordinates: string[]) => {
+    const reversedCoordinates = coordinates.reverse();
+    return reversedCoordinates;
+  };
+
+  const coordinates = createCoordinates(width, height);
+  const reversedCoordinates = createGrid(coordinates);
+
+  return (
+    <>
+      {width && height && (
+        <div
+          style={{
+            height: `${width * 5}rem`,
+            width: `${height * 5}rem`,
+            maxWidth: "80rem",
+            maxHeight: "80rem",
+            margin: "auto",
+            display: "grid",
+            gridTemplateColumns: `repeat(${width}, minmax(0, 1fr))`,
+            direction: "rtl",
+          }}
+        >
+          {reversedCoordinates.map((value: any) => (
+            <div
+              key={value}
+              className="border border-gray-500 text-center align-baseline text-tiny"
+              data-coordinates={value}
+            >
+              {value}
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MarsGrid;
