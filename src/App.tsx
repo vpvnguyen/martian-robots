5128b28b3b44b76b764587576c3bfb71ddde2ae6
vpvnguyen <vpvnguyen@gmail.com>
2021-04-16 21:00:56 -1000

    import and render RobotContextProvider and Robot component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import InstructionsForm from "./instructions/InstructionsForm";
 import MarsGrid from "./mars/MarsGrid";
+import Robot from "./robot/Robot";
 import { InstructionsContextProvider } from "./instructions/InstructionsContext";
+import { RobotContextProvider } from "./robot/RobotContext";
 
 const App = () => {
   return (
     <div className="h-screen w-full container mx-auto">
       <InstructionsContextProvider>
-        <div className="w-3/4 mx-auto">
-          <InstructionsForm />
-        </div>
+        <RobotContextProvider>
+          <div className="w-3/4 mx-auto">
+            <InstructionsForm />
+          </div>
 
-        <div className="lg:container lg:mx-auto overflow-x-auto">
-          <MarsGrid />
-        </div>
+          <div className="lg:container lg:mx-auto overflow-x-auto">
+            <MarsGrid />
+            <Robot />
+          </div>
+        </RobotContextProvider>
       </InstructionsContextProvider>
     </div>
   );
